d6cf7d30e282cdf73ff27e06024197fd22a7a09e
Sherazahmed1 <sherazahmed.co@gmail.com>
2021-04-05 21:03:44 +0500

    build directory url updated

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static('./dist/blackthorn-test'));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'/dist/blackthorn-test/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/blackthorn-test/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
